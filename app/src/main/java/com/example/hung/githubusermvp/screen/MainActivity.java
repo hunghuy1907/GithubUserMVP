966424d2976366799be6c969f83f642264848a05
huyhung1907 <hunghuy1907@gmail.com>
2018-08-08 15:41:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hung/githubusermvp/screen/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hung.githubusermvp.screen;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hung.githubusermvp.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "HOMEFRAGMENT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
